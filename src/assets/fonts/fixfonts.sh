49d5a9db6a38d9eeb80dbf7b46162d04317e5d56
zegiha <leeseoyul070814@gmail.com>
2024-07-01 21:23:00 +0900

    design: apply pretendard

---
+++ src/assets/fonts/fixfonts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# fixfonts.sh
+
+folder="$1"
+
+if [[ -d "$folder" && ! -z "$folder" ]]; then
+  pushd "$folder" || exit 1
+  for file in *.ttf; do
+    normalized="${file//-/_}"
+    normalized=$(echo "$normalized" | tr '[:upper:]' '[:lower:]')
+    if [[ "$file" != "$normalized" ]]; then
+      mv "$file" "$normalized"
+    fi
+  done
+  popd || exit 1
+else
+  if [[ -d "$folder" ]]; then
+    echo "Check"
+  else
+    echo "$folder"
+  fi
+  echo "Usage: $0 <folder>"
+fi
